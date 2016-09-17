371ef9a6113f4d3891f398242d62464041d3df36
shenjiaqi <1029393956@qq.com>
2016-09-17 20:10:42 +0800

    do train age model only

--- train/rf/RFTrainer.py
+++ train/rf/RFTrainer.py
@@ -3,6 +3,8 @@ import os
 from os.path import join
 import sys
 
+#from train import FeatureLoader
+
 sys.path.append("..")
 
 from FeatureLoader import FeatureLoader
@@ -16,11 +18,21 @@ class RFTrainer:
         self.age_random_forest = RandomForestClassifier()
         self.gender_random_forest = RandomForestClassifier()
         self.feature_loader = FeatureLoader()
+        self.age_feature = None
+        self.gender_feature = None
+
+    def load_age_feature(self, feature_dir):
+        self.age_feature, self.age_target = \
+            self.feature_loader.load_age_feature(feature_dir=feature_dir)
+
+    def load_gender_featur(self, feature_dir):
+        self.gender_feature, self.gender_target = \
+            self.feature_loader.load_gender_feature(feature_dir=feature_dir)
 
     def handle_age_feature(self):
         self.age_random_forest
+
     def train_age(self):
-        self.feature_loader.scan_feature()
         self.age_random_forest.fit(self.age_feature, self.age_target)
 
     def train_gender(self):
@@ -33,18 +45,16 @@ class RFTrainer:
         return self.gender_random_forest.predict_proba(feature)
 
     def save_age_model(self, model_dir):
-        s = pickle.dumps(self.age_random_forest)
         if not os.path.exists(model_dir):
             os.makedirs(model_dir)
         with open(join(model_dir, 'age_model'), 'w') as f:
-            f.write(s)
+            pickle.dump(self.age_random_forest, f)
 
     def save_gender_model(self, model_dir):
-        s = pickle.dumps(self.gender_random_forest)
         if not os.path.exists(model_dir):
             os.makedirs(model_dir)
         with open(join(model_dir, 'gender_model'), 'w') as f:
-            f.write(s)
+            pickle.dump(self.gender_random_forest, f)
 
     def save_model(self, model_dir):
         self.save_age_model(model_dir)
@@ -62,33 +72,19 @@ class RFTrainer:
         self.load_age_model(model_dir)
         self.load_gender_model(model_dir)
 
-zero_cnt = 0
-all_cnt = 0
-def handler_feature(tag, feature):
-    global zero_cnt
-    global all_cnt
-    all_cnt += 1
-    zero_cnt += 1
-    for i in feature:
-        if i > 1e-8:
-            zero_cnt -= 1
-            break
-
 if __name__ == '__main__':
     with open('data.json', 'r') as f:
         conf = json.load(f)
         base_dir = conf['base_dir']
         feature_dir = join(base_dir, conf['norm_feature_dir'])
 
-        feature_loader = FeatureLoader()
-        feature_loader.scan_feature(feature_dir, 'age', handle_feature=handler_feature)
-        global zero_cnt, all_cnt
-        print zero_cnt, all_cnt, float(zero_cnt) / all_cnt
-        '''
         rf_trainer = RFTrainer()
-        rf_trainer.load_feature(feature_dir=feature_dir)
+        print 'load feature'
+        rf_trainer.load_age_feature(feature_dir=feature_dir)
+        print 'train model'
         rf_trainer.train_age()
-        rf_trainer.train_gender()
-        rf_trainer.save_model(join(base_dir, conf['model_dir']))
-        '''
+        #rf_trainer.train_gender()
+        print 'save model'
+        rf_trainer.save_age_model(join(base_dir, conf['model_dir']))
+
 
