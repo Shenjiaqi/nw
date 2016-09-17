5e8851d0e99cf91fe24e4b24420eb5194ecb37ed
shenjiaqi <1029393956@qq.com>
2016-09-17 14:30:54 +0800

    Add rf trainer eval

---
+++ train/rf/eval.py
@@ -0,0 +1,45 @@
+import json
+import os
+from os.path import join
+
+import pickle
+
+from RFTrainer import RFTrainer
+
+if __name__ == '__main__':
+    with open('data.json', 'r') as f:
+        conf = json.load(f)
+        base_dir = conf['base_dir']
+        feature_dir = join(base_dir, conf['feature_dir'])
+        model_dir = join(base_dir, conf['model_dir'])
+        eval_dir = join(base_dir, conf['eval_dir'])
+        rf_trainer = RFTrainer()
+        rf_trainer.load_model(model_dir)
+        # age, gender
+        for t in os.listdir(feature_dir):
+            # age/1, age/2, gender/1, ...
+            for c in os.listdir(join(feature_dir, t)):
+                # age/1/feature
+                for file in os.listdir(join(feature_dir, t, c)):
+                    print join(feature_dir, t, c, file)
+                    with open(join(feature_dir, t, c, file), 'r') as f:
+                        if not os.path.exists(join(eval_dir, t, c)):
+                            os.makedirs(join(eval_dir, t, c))
+                        with open(join(eval_dir, t, c, file), 'w') as out_f:
+                            pred_cnt = [0 for i in range(0, 8)]
+                            pred_sum = 0
+                            for l in f.readlines():
+                                feature_line = [float(i) for i in l.split(',')]
+                                if t == 'age':
+                                    pred_result = rf_trainer.predict_age([feature_line])[0]
+                                else:
+                                    pred_result = rf_trainer.predict_gender([feature_line])[0]
+                                out_f.write(','.join([str(x) for x in pred_result]) + '\n')
+                                jdx = 0
+                                for j in xrange(0, len(pred_result)):
+                                    if pred_result[jdx] < pred_result[j]:
+                                        jdx = j
+                                pred_sum += 1
+                                pred_cnt[jdx] += 1
+                            print t, c, file
+                            print ','.join([str(x / float(pred_sum)) for x in pred_cnt])
