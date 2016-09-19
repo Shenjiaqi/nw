5abbf8abbae8f20dc266b395e7990e45af7dd8cb
shenjiaqi <1029393956@qq.com>
2016-09-19 12:58:20 +0800

    change eval log

--- train/rf/eval.py
+++ train/rf/eval.py
@@ -6,13 +6,31 @@ from os.path import join
 import pickle
 
 from RFTrainer import RFTrainer
+from train import FeatureLoader
 
 if __name__ == '__main__':
     with open('data.json', 'r') as f:
         conf = json.load(f)
         base_dir = conf['base_dir']
-        feature_dir = join(base_dir, conf['norm_feature_dir'])
         model_dir = join(base_dir, conf['model_dir'])
+        feature_loader = FeatureLoader()
+        feature_list = feature_loader.load_age_data_less_than_n(base_dir=base_dir, n=10000)
+        rf_trainer = RFTrainer()
+        rf_trainer.load_model(model_dir=model_dir)
+        for i in feature_list:
+            cnt = [0, 0]
+            for j in feature_list[i]:
+                pred = rf_trainer.predict_age(j)
+                mk = 0
+                for k in len(pred):
+                    if pred[mk] < pred[k]:
+                        mk = k
+                cnt[mk] += 1
+                if (cnt[0] + cnt[1] % 100) == 0:
+                    print i, cnt
+            print "#####", i, cnt
+        '''
+        feature_dir = join(base_dir, conf['norm_feature_dir'])
         eval_dir = join(base_dir, conf['eval_dir'])
         rf_trainer = RFTrainer()
         rf_trainer.load_model(model_dir)
@@ -47,3 +65,4 @@ if __name__ == '__main__':
                                         print ','.join([str(x / float(pred_sum + 0.001)) for x in pred_cnt])
                             print t, c, file
                             print ','.join([str(x / float(pred_sum + 0.001)) for x in pred_cnt])
+        '''
